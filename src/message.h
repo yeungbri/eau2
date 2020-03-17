3cd67b41438f1ef24ffc2eaa08ee88a788a01694
Brian Yeung <brianyeung2011@gmail.com>
2020-03-17 13:24:55 -0400

    move messages to cpp

--- src/message.h
+++ src/message.h
@@ -3,36 +3,14 @@
  * Emails: yeung.bri@husky.neu.edu, gao.d@husky.neu.edu
  */
 
-//lang::CwC
+//lang::Cpp
 
 #pragma once
 #include "string.h"
 #include <netinet/in.h>
 #include "serial.h"
-
-class StringArray {
-public:
-   String* vals_;
-   size_t len_;
-
-   StringArray() { }
-
-   StringArray(String* vals, size_t len) {
-       vals_ = vals;
-       len_ = len;
-   }
-};
-
-class DoubleArray {
-public:
-    double* vals_;
-    size_t len_;
-
-    DoubleArray(double* vals, size_t len) {
-        vals_ = vals;
-        len_ = len;
-    }
-};
+#include <vector>
+#include <string>
 
 enum class MsgKind {
     Ack, Nack, Put,
@@ -40,7 +18,7 @@ enum class MsgKind {
     Kill, Register, Directory
 };
 
-class Message : public Object {
+class Message {
 public:
     MsgKind kind_;  // the message kind
     size_t sender_; // the index of the sender node
@@ -62,9 +40,9 @@ public:
 
 class Status : public Message {
 public:
-    String* msg_; // owned
+    std::string msg_; // owned
 
-    Status(MsgKind kind, size_t sender, size_t target, size_t id, String* msg) : Message(kind, sender, target ,id) {
+    Status(MsgKind kind, size_t sender, size_t target, size_t id, std::string msg) : Message(kind, sender, target ,id) {
         msg_ = msg;
     };
 };
@@ -84,9 +62,9 @@ class Directory : public Message {
 public:
     size_t client_;
     size_t * ports_;  // owned
-    StringArray addresses_;  // owned; strings owned
+    std::vector<std::string> addresses_;  // owned; strings owned
 
-    Directory(MsgKind kind, size_t sender, size_t target, size_t id, size_t client, size_t* ports, StringArray addresses) : Message(kind, sender, target ,id) {
+    Directory(MsgKind kind, size_t sender, size_t target, size_t id, size_t client, size_t* ports, std::vector<std::string> addresses) : Message(kind, sender, target ,id) {
         client_ = client;
         ports_ = ports;
         addresses_ = addresses;
