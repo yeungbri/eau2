663973b6a46988c734e721f5664e995569fd04bd
Brian Yeung <brianyeung2011@gmail.com>
2020-03-23 18:05:46 -0400

    add comments to application

--- src/application.h
+++ src/application.h
@@ -3,58 +3,63 @@
  * Emails: yeung.bri@husky.neu.edu, gao.d@husky.neu.edu
  */
 
-//lang::Cpp
+// lang::Cpp
 
 #pragma once
-#include "dataframe.h"
-#include "kvstore.h"
-#include <string>
-#include <vector>
 #include <assert.h>
 
+#include <string>
+#include <vector>
+
+#include "dataframe.h"
+#include "kvstore.h"
+
+/** An application runs on a node and owns one local kvstore */
 class Application {
-public:
-  size_t idx_;
-  KVStore kv = KVStore();
+ public:
+  size_t idx_;             // index of node it is running on
+  KVStore kv = KVStore();  // local kvstore
 
-  Application(size_t idx) : idx_(idx) {
-  }
+  Application(size_t idx) : idx_(idx) {}
 
-  virtual void run_() { }
+  /** Invoke to run the application */
+  virtual void run_() {}
 
-  size_t this_node() {
-    return idx_;
-  }
+  size_t this_node() { return idx_; }
 };
 
+/** Trival application to test serialization on a single node */
 class Trivial : public Application {
-public:
-  Trivial(size_t idx) : Application(idx) { }
+ public:
+  Trivial(size_t idx) : Application(idx) {}
 
+  /** Creates a dataframe from all the numbers from 1 - 999999 and
+   * stores it onto the local kvstore. Then retrieves the stored 
+   * value and checks all values are there */
   void run_() {
-    size_t SZ = 1000*1000;
+    size_t SZ = 1000 * 1000;
     std::vector<double> vals;
     double sum = 0;
-    for (size_t i = 0; i < SZ; ++i){
-        vals.push_back(i);
-        sum += i;
+    for (size_t i = 0; i < SZ; ++i) {
+      vals.push_back(i);
+      sum += i;
     }
     Key key("triv", 0);
     DataFrame* df = DataFrame::fromArray(&key, &kv, vals);
     assert(df->get_double(0, 1) == 1);
-    
+
     Value val = kv.get(key);
     Deserializer dser(val.data());
-    DataFrame *df2 = DataFrame::deserialize(dser);
-    for (size_t i = 0; i < SZ; ++i)
-    {
+    DataFrame* df2 = DataFrame::deserialize(dser);
+    for (size_t i = 0; i < SZ; ++i) {
       assert(df2->get_double(0, i) == i);
       sum -= df2->get_double(0, i);
     }
     assert(sum == 0);
     std::cout << "SUCCESS" << std::endl;
-    
-    delete df; delete df2;
+
+    delete df;
+    delete df2;
   }
 };
 
@@ -63,7 +68,7 @@ public:
 //   Key main = Key("main", 0);
 //   Key verify = Key("verif", 0);
 //   Key check = Key("ck", 0);
- 
+
 //   Demo(size_t idx): Application(idx) { }
 
 //   void run_() override {
@@ -94,6 +99,7 @@ public:
 //   void summarizer() {
 //     DataFrame* result = kv.waitAndGet(verify);
 //     DataFrame* expected = kv.waitAndGet(check);
-//     std::cout << (expected->get_double(0,0)==result->get_double(0,0) ? "SUCCESS":"FAILURE") << "\n";
+//     std::cout << (expected->get_double(0,0)==result->get_double(0,0) ?
+//     "SUCCESS":"FAILURE") << "\n";
 //   }
 // };
